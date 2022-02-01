d093010d8e83c93f46d7131e60e001e17af4ef9c
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2022-02-01 13:14:42 -0300

    feat: add best solutions to scala Katas

--- scala/src/main/scala/codewars/Kata.scala
+++ scala/src/main/scala/codewars/Kata.scala
@@ -3,6 +3,10 @@ import Math.pow
 package codewars {
   object Kata {
     def boolToWord(boolean: Boolean): String = {
+      /*
+      * best solution:
+      this solution is good enough :D
+      * */
       if (boolean) "Yes" else "No"
     }
 
@@ -15,22 +19,34 @@ package codewars {
     }
 
     def pillars(number: Int, distance: Int, width: Int): Int = {
+      /*
+      * best solution:
+      (n-1) * d * 100 + ((n-2) max 0) * w
+      * */
       if (number == 1) 0 else (distance * 100) * (number - 1) + width * (number - 2)
     }
 
-    /*
-    * best solution:
-    this solution is good enough :D
-    * */
     def smallEnough(n: Seq[Int], l: Int): Boolean = {
+      /*
+      * best solution:
+      this solution is good enough :D
+      * */
       !n.exists(_ > l)
     }
 
     def squareSum(xs: List[Int]): Int = {
+      /*
+      * best solution:
+      xs.map(x => x*x).sum
+      * */
       if (xs.isEmpty) 0 else xs.map(pow(_, 2)).sum.asInstanceOf[Int]
     }
 
     def sumTwoSmallest(numbers: List[Int]): Int = {
+      /*
+      * best solution:
+      numbers.sorted.take(2).sum
+      * */
       numbers.min + numbers.patch(numbers.indexOf(numbers.min), Nil, 1).min
     }
   }
