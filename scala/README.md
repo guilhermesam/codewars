c64d8a1da618232f99bf64b4ab37a36f5a3f4a3f
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2022-02-01 12:37:26 -0300

    kata: sum two smallest

--- scala/README.md
+++ scala/README.md
@@ -6,3 +6,10 @@ number of pillars (≥ 1);
 distance between pillars (10 - 30 meters);
 width of the pillar (10 - 50 centimeters).
 Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+## Sum of two lowest positive integers
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
--- scala/src/main/scala/codewars/Kata.scala
+++ scala/src/main/scala/codewars/Kata.scala
@@ -2,16 +2,20 @@ import Math.pow
 
 package codewars {
   object Kata {
-    def squareSum(xs: List[Int]): Int = {
-      if (xs.isEmpty) 0 else xs.map(pow(_, 2)).sum.asInstanceOf[Int]
-    }
-
     def boolToWord(boolean: Boolean): String = {
       if (boolean) "Yes" else "No"
     }
 
     def pillars(number: Int, distance: Int, width: Int): Int = {
       if (number == 1) 0 else (distance * 100) * (number - 1) + width * (number - 2)
+    }
+
+    def squareSum(xs: List[Int]): Int = {
+      if (xs.isEmpty) 0 else xs.map(pow(_, 2)).sum.asInstanceOf[Int]
+    }
+
+    def sumTwoSmallest(numbers: List[Int]): Int = {
+      numbers.min + numbers.patch(numbers.indexOf(numbers.min), Nil, 1).min
     }
   }
 }
